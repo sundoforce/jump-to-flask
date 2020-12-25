8e0e1c88f42b49d211735c6a7d9527205f781dc4
sdk <admin@sundo.kim>
2020-12-25 23:18:25 +0900

    test: models.py

--- pybo/models.py
+++ pybo/models.py
@@ -6,7 +6,7 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
     user = db.relationship('User', backref=db.backref('question_set'))
 
 
@@ -16,7 +16,7 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
     user = db.relationship('User', backref=db.backref('answer_set'))
 
 
